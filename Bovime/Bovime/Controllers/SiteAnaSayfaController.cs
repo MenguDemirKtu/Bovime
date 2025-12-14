69b4fc610d6c871ca2f50d957aaf2e5c3961335e
Mengü Demir <mengu.demir@egitimkentyazilim.com>
2025-12-14 23:17:14 +0300

    Son ye nilikler
    ...

---
+++ Bovime/Bovime/Controllers/SiteAnaSayfaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bovime.Controllers
+{
+    public class SiteAnaSayfaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
