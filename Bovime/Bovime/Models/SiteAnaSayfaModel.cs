9e7d6f5b4cd0ef3fd8184306b1d254199397abe8
Mengü Demir <mengu.demir@egitimkentyazilim.com>
2026-01-04 14:58:02 +0300

    Bovime Değişiklikleri
    ...

--- Bovime/Bovime/Models/SiteAnaSayfaModel.cs
+++ Bovime/Bovime/Models/SiteAnaSayfaModel.cs
@@ -9,6 +9,7 @@ namespace Bovime.Models
         public List<ReklamKusagi1AYRINTI> reklamKusagi1 { get; set; }
 
         public List<ReklamKusagi2AYRINTI> reklamKusagi0 { get; set; }
+        public List<ReklamKusagi3AYRINTI> reklamKusagi2 { get; set; }
 
         public List<SektorAYRINTI> anaSektorler { get; set; }
 
@@ -19,7 +20,8 @@ namespace Bovime.Models
             reklamKusagi1 = new List<ReklamKusagi1AYRINTI>();
             anaSektorler = new List<SektorAYRINTI>();
             reklamKusagi0 = new List<ReklamKusagi2AYRINTI>();
-            this.firmaGruplari = new List<FirmaGrubuAYRINTI>();
+            firmaGruplari = new List<FirmaGrubuAYRINTI>();
+            reklamKusagi2 = new List<ReklamKusagi3AYRINTI>();
         }
         public async Task veriCek()
         {
@@ -30,6 +32,7 @@ namespace Bovime.Models
                 anaSektorler = await vari.SektorAYRINTIs.OrderBy(p => p.sektorAdi).ToListAsync();
                 reklamKusagi0 = await vari.ReklamKusagi2AYRINTIs.OrderBy(p => p.sirasi).ToListAsync();
                 firmaGruplari = await FirmaGrubuAYRINTI.ara(vari);
+                reklamKusagi2 = await ReklamKusagi3AYRINTI.ara(vari);
                 await Genel.yenile(vari);
             }
         }
